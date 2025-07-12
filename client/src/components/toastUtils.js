0c39b6e0b45108059e25bcc9dd40d2e7afebded8
Abhinav <abhinavsurya.kolla@gmail.com>
2025-07-12 14:24:48 +0530

    deployment error fix

--- client/src/components/toastUtils.js
+++ client/src/components/toastUtils.js
@@ -3,3 +3,19 @@ import { toast } from 'react-toastify';
 export const showSuccess = (msg) => toast.success(msg, { position: 'top-right', autoClose: 2000 });
 export const showError = (msg) => toast.error(msg, { position: 'top-right', autoClose: 2000 });
 export const showInfo = (msg) => toast.info(msg, { position: 'top-right', autoClose: 2000 }); 
+
+export const showToast = (message, type = 'info') => {
+  const options = { position: 'top-right', autoClose: 2000 };
+  
+  switch (type) {
+    case 'success':
+      return toast.success(message, options);
+    case 'error':
+      return toast.error(message, options);
+    case 'warning':
+      return toast.warning(message, options);
+    case 'info':
+    default:
+      return toast.info(message, options);
+  }
+}; 
